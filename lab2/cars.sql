548396daa64b75d8e188a3d0308079258d26b916
Kamille <kamille.tipan@gmail.com>
2022-03-09 01:23:08 -0500

    Moved to mac version, starting to understand more

---
+++ lab2/cars.sql
@@ -0,0 +1,14 @@
+(20) statement aborts at 19: [INSERT INTO Cars SELECT car_id,year,make,model,timestamp FROM original;] datatype mismatch
+(1) near "cars": syntax error in "drop cars
+;"
+(1) near "Cars": syntax error in "DROP Cars;"
+(1) near "ls": syntax error in "ls
+;"
+(1) table Cars has 6 columns but 5 values were supplied in "INSERT INTO Cars SELECT car_id,year,make,model,timestamp FROM original;"
+(1) near "SELECT": syntax error in "CREATE TABLE Car_Score(SELECT Car_ID FROM Cars);"
+(1) near "SELECT": syntax error in "CREATE TABLE Car_Score SELECT Car_ID FROM Cars;"
+(1) no such table: Carss in "INSERT INTO Carss SELECT car_id,year,make,model,timestamp FROM original;"
+(1) near "(": syntax error in "CREATE TABLE carss AS (SELECT car_id FROM cars);"
+(1) near "(": syntax error in "CREATE TABLE carss AS(SELECT car_id FROM cars);"
+(1) near "carss": syntax error in "SELECT * carss;"
+(1) near "carss": syntax error in "SELECT * carss;"
